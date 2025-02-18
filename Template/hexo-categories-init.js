164da0f8636d555ee94e2d1d314f31ed19f47446
FreeFunk <2917426372@qq.com>
2025-02-18 11:01:21 +0800

    文章提交: 2025-02-18 11:01:21

---
+++ Template/hexo-categories-init.js
@@ -0,0 +1,16 @@
+---
+title: 
+date: <% tp.date.now("YYYY-MM-DD HH:mm:ss") %>
+top_img: false #是否显示顶部图
+type: "categories" #页面类型，必须为 tags
+comments: false #是否显示评论
+aside: false #是否显示右侧作者栏
+updated:
+description:
+keywords:
+mathjax:
+katex:
+aplayer:
+highlight_shrink:
+top_single_background:
+---
--- Template/hexo-post-init.js
+++ Template/hexo-post-init.js
@@ -1,7 +1,7 @@
 ---
-title: 'Obsidian同步Hexo部署' #文章标题
-date: 2025-02-18 11:00:04 #文章创建日期
-updated: 2025-02-18 11:00:04 #文章更新日期
+title: '<% tp.file.title %>' #文章标题
+date: <% tp.date.now("YYYY-MM-DD HH:mm:ss") %> #文章创建日期
+updated: <% tp.date.now("YYYY-MM-DD HH:mm:ss") %> #文章更新日期
 categories: #文章分类
 tags: #文章标签
 keywords: #文章关键字
@@ -12,12 +12,7 @@ copyright_author: 'FreeFunk' #文章版权模块的文章作者
 copyright_author_href: 'https://free-funk-github-io.vercel.app' #文章版权模块的文章作者链接
 main_color: '#e1efe1' #文章主色，必须是16进制颜色且有6位，不可缩减，例如#ffffff 不可写成#fff
 highlight_shrink: true #配置代码框是否展开(true/false)(默认为设置中 highlight_shrink 的配置)
-abbrlink: 72c0a14c #随机字符串用作文章博客唯一键
+abbrlink: <%* date = tp.date.now("YYYYMMDDhhmmss"); %><% tp.user.hash_8c_2(date) %> #随机字符串用作文章博客唯一键
 mathjax: true #文章分类
 thumbnail: false
 ---
-测试内容
-
-
-![[Obsidian同步Hexo部署/file-20250218105514089.png]]
-
