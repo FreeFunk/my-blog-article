3952b9cac6b32195a7c47f99f768170fb53a16c4
FreeFunk <2917426372@qq.com>
2025-03-08 21:08:44 +0800

    文章提交: 2025-03-08 21:08:44

--- Template/hexo-post-init.js
+++ Template/hexo-post-init.js
@@ -9,7 +9,7 @@ description: #文章描述
 top_img: #文章顶部图片
 cover: #文章缩略图(如果没有设置 top_img,文章页顶部将显示缩略图，可设为 false/图片地址/留空)
 copyright_author: 'FreeFunk' #文章版权模块的文章作者 
-copyright_author_href: 'https://free-funk-github-io.vercel.app' #文章版权模块的文章作者链接
+copyright_author_href: 'https://blog.freefunk.pp.ua' #文章版权模块的文章作者链接
 main_color: '#e1efe1' #文章主色，必须是16进制颜色且有6位，不可缩减，例如#ffffff 不可写成#fff
 highlight_shrink: true #配置代码框是否展开(true/false)(默认为设置中 highlight_shrink 的配置)
 abbrlink: <%* date = tp.date.now("YYYYMMDDhhmmss"); %><% tp.user.hash_8c_2(date) %> #随机字符串用作文章博客唯一键
